730dbe37487e6922c966a6452aef1568350df60b
hyuk2 <hyuk2000s@gmail.com>
2025-03-12 11:32:57 +0900

    feat: 20250310 shopping list add items function ended

---
+++ shoppinglist/src/components/button.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@mui/material";
+
+export default function ClickButton(){
+  return(
+    <Button variant="contained" color="secondary">
+      클릭하세요
+    </Button>
+  );
+}
--- shoppinglist/src/main.tsx
+++ shoppinglist/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from './App2.tsx'
+// import './index.css' -> 얘 사용 안하고, vite에서 미리 정의된 스타일 사용 안할거임
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
