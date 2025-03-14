7b3536311edf5cb57fba85213789d3666e7b9eab
hyuk2 <hyuk2000s@gmail.com>
2025-03-14 13:23:10 +0900

    feat: 20250311 01-starting-project started

--- routerapp/src/App.tsx
+++ routerapp/src/App.tsx
@@ -1,11 +1,33 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Home from './components/Home'
+import PageNotFound from './components/PageNotFound'
+import Contact from './components/Contact'
+
 import './App.css'
+import ContactSeoul from './components/ContactSeoul'
+import ContactLondon from './components/ContactLondon'
+
 
 function App() {
  
   return (
     <>
-    
+      <BrowserRouter>
+        <nav>
+          <Link to="/">Home</Link>{' | '}
+          <Link to="/contact">Contact</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path='/contact' element={<Contact/>}>
+            <Route path="seoul" element={<ContactSeoul/>}>
+            </Route>
+            <Route path="london" element={<ContactLondon/>}>
+            </Route>
+          </Route>
+          <Route path="*" element={<PageNotFound/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
