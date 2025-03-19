45dce32126f310a395b8ac7aa95974aca85d4449
hyuk2 <hyuk2000s@gmail.com>
2025-03-19 19:03:17 +0900

    feat: 20250317 context API lesson

---
+++ kpoplistcontext/src/components/Main.jsx
@@ -0,0 +1,12 @@
+import Button from './Button';
+import KPopList from './KPopList';
+
+export default function Main(){
+  return(
+    <main>
+      <Button>레드벨벳 노래 찾기</Button>
+      <Button>남자 아티스트 노래 찾기</Button>
+      <Button>여자 아티스트 노래 찾기</Button>
+    </main>
+  )
+}
