88332f54426e71ced2fea96840a8bba5e5dd71e0
hyuk2 <hyuk2000s@gmail.com>
2025-03-21 12:30:41 +0900

    feat: 20250319 useReducer loginForm

--- usereducerapp/src/App.jsx
+++ usereducerapp/src/App.jsx
@@ -1,13 +1,24 @@
 import './App.css'
-
+import {  useReducer } from 'react';
+import LoginForm from './components/LoginForm';
+import Reducer from './reducer/Reducer';
 
 function App() {
-
+  const [ state, dispatch ] = useReducer(Reducer, { isLogin: false, message : ''});
 
   return (
-    <>
-    
-    </>
+    <div>
+      {state.isLogin ? (
+        <>
+          <strong>welcome</strong>
+          <button onClick={() => dispatch({type: "LOGOUT"})}>로그아웃</button>
+        </>
+      ): ( 
+      <>
+        <LoginForm state={state} dispatch={dispatch}/>
+      </>
+      )}
+    </div>  
   )
 }
 
